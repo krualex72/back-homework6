851c8354c14de9c2ed59c285bf01e32a85f5d888
krutikov-alex <krutikov-alex@ya.ru>
2022-05-25 17:11:15 +0300

    Homework 6 in Progress

--- src/main/java/homework6/Main.java
+++ src/main/java/homework6/Main.java
@@ -1,5 +1,7 @@
 package homework6;
 
+import homework6.db.dao.CategoriesMapper;
+import homework6.db.model.CategoriesExample;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -11,9 +13,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         SqlSessionFactory sqlSessionFactory;
 //NyBatis Configuration file
-        String resource = "mybatisConfig.xml";
+        String resource = "mybatis-config.xml";
         InputStream is = Resources.getResourceAsStream(resource);
         sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
     }
-}
 
+//    public static Integer countNumberOfAllCategories() {
+//        CategoriesMapper categoriesMapper = CategoriesMapper(resource);
+//        CategoriesExample example = new CategoriesExample();
+//        return Math.toIntExact(categoriesMapper.countByExample(example));
+//    }
+}
